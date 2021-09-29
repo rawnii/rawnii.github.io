23ba0fe18579a59b40ccf7176c4fec37b2291c1f
Rawnii <ryanmooney22@gmail.com>
2021-09-29 08:41:38 -0600

    f

---
+++ games/Num Guesser Game/script.js
@@ -0,0 +1,35 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+
+let generateTarget = () => {
+    return Math.floor(Math.random() * 9);  
+};
+
+
+let compareGuesses = (human, computer, target) => {
+    const userG = Math.abs(target - human);
+    const computerG = Math.abs(target - computer);
+    return userG <= computerG;
+
+    /*if (userG <= computerG) {
+        updateScore('human');
+    } else {
+        updateScore('computer');
+    }*/
+};
+
+
+let updateScore = winner => {
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    }
+};
+
+
+function advanceRound() {
+    currentRoundNumber += 1;
+};
